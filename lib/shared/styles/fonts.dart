dc3ba0fd9d117ea707a54bebd3aedc8d81075865
jasonrivalino <13521008@std.stei.itb.ac.id>
2025-05-14 21:44:31 +0700

    feat: login selection page

---
+++ lib/shared/styles/fonts.dart
@@ -0,0 +1,5 @@
+//  This file contains the font styles used in the application.
+class AppFonts {
+  static const String fontMedium = 'Roboto-Medium';
+  static const String fontBold = 'Roboto-Bold';
+}
