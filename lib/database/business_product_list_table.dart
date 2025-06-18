84df5af08248fb9ded7e8b62db4d27ead78b3f71
jasonrivalino <13521008@std.stei.itb.ac.id>
2025-06-18 20:43:10 +0700

    data: adding seeding for documentation

--- lib/database/business_product_list_table.dart
+++ lib/database/business_product_list_table.dart
@@ -43,4 +43,31 @@ final List<Map<String, dynamic>> businessProductListTable = [
 
   {'business_id': 18, 'product_id': 31, 'add_ons_id': null},
   {'business_id': 18, 'product_id': 32, 'add_ons_id': null},
+
+  {'business_id': 12, 'product_id': 1, 'add_ons_id': 1},
+  {'business_id': 12, 'product_id': 1, 'add_ons_id': 2},
+  {'business_id': 12, 'product_id': 1, 'add_ons_id': 3},
+
+  {'business_id': 12, 'product_id': 2, 'add_ons_id': 4},
+  {'business_id': 12, 'product_id': 2, 'add_ons_id': 5},
+  
+  {'business_id': 12, 'product_id': 3, 'add_ons_id': null},
+  {'business_id': 12, 'product_id': 4, 'add_ons_id': null},
+  {'business_id': 12, 'product_id': 5, 'add_ons_id': null},
+  {'business_id': 12, 'product_id': 6, 'add_ons_id': null},
+  {'business_id': 12, 'product_id': 7, 'add_ons_id': null},
+  {'business_id': 12, 'product_id': 8, 'add_ons_id': null},
+  {'business_id': 12, 'product_id': 9, 'add_ons_id': null},
+  {'business_id': 12, 'product_id': 10, 'add_ons_id': null},
+
+  {'business_id': 22, 'product_id': 11, 'add_ons_id': null},
+  {'business_id': 22, 'product_id': 12, 'add_ons_id': null},
+  {'business_id': 22, 'product_id': 13, 'add_ons_id': null},
+  {'business_id': 22, 'product_id': 14, 'add_ons_id': null},
+  {'business_id': 22, 'product_id': 15, 'add_ons_id': null},
+  {'business_id': 22, 'product_id': 16, 'add_ons_id': null},
+  {'business_id': 22, 'product_id': 17, 'add_ons_id': null},
+  {'business_id': 22, 'product_id': 18, 'add_ons_id': null},
+  {'business_id': 22, 'product_id': 19, 'add_ons_id': null},
+  {'business_id': 22, 'product_id': 20, 'add_ons_id': null},
 ];
